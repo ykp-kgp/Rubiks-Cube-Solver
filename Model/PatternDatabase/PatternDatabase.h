df538f65416e16a9e8f3fd5e59b95c92f0ee68b7
ykp-kgp <117109746+ykp-kgp@users.noreply.github.com>
2023-06-29 12:31:40 +0530

    Add files via upload

---
+++ Model/PatternDatabase/PatternDatabase.h
@@ -0,0 +1,19 @@
+//
+// Created by Yogesh Kumar Pandey.
+//
+
+#ifndef RUBIKS_CUBE_SOLVER_PATTERNDATABASE_H
+#define RUBIKS_CUBE_SOLVER_PATTERNDATABASE_H
+
+template<typename T>
+class PatternDatabaseEstimate {
+private:
+
+public:
+    int getEstimate(T rubiksCube) {
+        return 0;
+    }
+
+};
+
+#endif //RUBIKS_CUBE_SOLVER_PATTERNDATABASE_H
